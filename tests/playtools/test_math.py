218506b992bc39b29e929791cf87314eed246000
Matej Murín <matejmurin01@gmail.com>
2024-12-17 23:07:25 +0100

    Added test for test_math.

--- tests/playtools/test_math.py
+++ tests/playtools/test_math.py
@@ -1,4 +1,5 @@
-from gorqflow.playtools import add_one, sub_one
+import pytest
+from gorqflow.playtools import add_one, sub_one, divide
 
 
 def test_add_one():
@@ -7,3 +8,12 @@ def test_add_one():
 
 def test_sub_one():
     assert sub_one(1) == 0
+
+
+def test_divide_ok():
+    assert divide(15., 3.) == 5.
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(15., 0.)
