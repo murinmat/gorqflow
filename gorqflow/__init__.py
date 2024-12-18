d3f6983918176bf714506b2bcbb3fa00d40b8f24
Matej Murín <matejmurin01@gmail.com>
2024-12-18 12:01:45 +0100

    Moved version resolution to init file.

--- gorqflow/__init__.py
+++ gorqflow/__init__.py
@@ -1,6 +1,1 @@
-import importlib.metadata
-
-try:
-    __version__ = importlib.metadata.version('gorqflow')
-except importlib.metadata.PackageNotFoundError:
-    __version__ = 'unknown'
+__version__ = '0.3.0-dev'
